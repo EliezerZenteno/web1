78910cd23b7dcfe96d4e9c88d6eb3b11a0308bbf
Eliezer <elizenteno87@gmail.com>
2025-08-28 14:13:55 -0400

    se agrego, en el nav, whyplay y arreglo, gallery

--- web1/web1/urls.py
+++ web1/web1/urls.py
@@ -5,5 +5,6 @@ from core import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('gallery/',views.gallery,name="gallery"),
+    path('whyplay/',views.whyplay,name="whyplay"),
     path('admin/', admin.site.urls),
 ]
